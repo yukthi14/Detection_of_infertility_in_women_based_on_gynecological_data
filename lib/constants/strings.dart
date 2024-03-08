f3e69b037fc8ca5e8de0c26a40fc3afe0a9a1ce0
Vishwaajeet Bharti <71969138+vishwajeetbharti@users.noreply.github.com>
2024-03-08 18:56:38 +0530

    Expaning Input Name with Full form

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -1,18 +1,18 @@
 class Strings {
   static const String heading = "AMC Engineering College";
   static const String Age = "Age";
-  static const String bmi = "Bmi";
-  static const String hb = "hb";
+  static const String bmi = "Body Mass Index";
+  static const String hb = "Hemoglobin Concentration";
   static const String cycleLength = "Cycle Length";
   static const String marriageStatus = "Marriage Status";
   static const String noOfAbortion = "Number Of Abortion";
-  static const String fsh = "fsh";
+  static const String fsh = "Follicle-Stimulating Hormone";
   static const String lh = "lh";
-  static const String ths = "ths";
-  static const String amh = "amh";
+  static const String ths = "Thyroid-Simulating Hormone";
+  static const String amh = "Anti-Mullerian Hormone";
   static const String vit = "vit";
-  static const String prg = "prg";
+  static const String prg = "Progesterone";
   static const String sugar = "Sugar";
-  static const String bp = "bp";
+  static const String bp = "Blood Pressure";
   static const String endometrium = "Endometrium";
 }
