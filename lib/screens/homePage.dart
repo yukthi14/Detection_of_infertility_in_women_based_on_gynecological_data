59e05edece9f0df0ea8b2bec80b882d8e09a5e4a
yukthi14 <109909210+yukthi14@users.noreply.github.com>
2024-03-10 14:03:09 +0530

    homePage contains project title.

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -1,11 +1,8 @@
-import 'package:detection_of_infertility_in_women_based_on_gynecological_data_using_supervised_and_unsupervised_method/constants/colors.dart';
+import 'package:animate_do/animate_do.dart';
+import 'package:detection_of_infertility_in_women_based_on_gynecological_data_using_supervised_and_unsupervised_method/constants/textStyle.dart';
 import 'package:flutter/material.dart';
 
 import '../constants/strings.dart';
-import '../constants/textStyle.dart';
-import '../functions/linearRegressionApi.dart';
-import '../models/linearModel.dart';
-import '../widgets/inputField.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -15,325 +12,32 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  String result = '';
-
-  late FocusNode ageFocus;
-  late FocusNode bmiFocus;
-  late FocusNode hbFocus;
-  late FocusNode cycleFocus;
-  late FocusNode marriageFocus;
-  late FocusNode abortionFocus;
-  late FocusNode fshFocus;
-  late FocusNode lhFocus;
-  late FocusNode thsFocus;
-  late FocusNode amhFocus;
-  late FocusNode vitFocus;
-  late FocusNode prgFocus;
-  late FocusNode sugarFocus;
-  late FocusNode bpFocus;
-  late FocusNode endFocus;
-  late FocusNode buttonFocus;
-  final TextEditingController textEditingControllerAge =
-      TextEditingController();
-  final TextEditingController textEditingControllerBmi =
-      TextEditingController();
-  final TextEditingController textEditingControllerHb = TextEditingController();
-  final TextEditingController textEditingControllerCycle =
-      TextEditingController();
-  final TextEditingController textEditingControllerMarriage =
-      TextEditingController();
-  final TextEditingController textEditingControllerAbortion =
-      TextEditingController();
-  final TextEditingController textEditingControllerFsh =
-      TextEditingController();
-  final TextEditingController textEditingControllerLh = TextEditingController();
-  final TextEditingController textEditingControllerThs =
-      TextEditingController();
-  final TextEditingController textEditingControllerAmh =
-      TextEditingController();
-  final TextEditingController textEditingControllerVit =
-      TextEditingController();
-  final TextEditingController textEditingControllerPrg =
-      TextEditingController();
-  final TextEditingController textEditingControllerSugar =
-      TextEditingController();
-  final TextEditingController textEditingControllerBp = TextEditingController();
-  final TextEditingController textEditingControllerEndometrium =
-      TextEditingController();
-  bool isHover = false;
-  bool buttonFocused = false;
-
-  int getItemCountPerRow(BuildContext context) {
-    double minTileWidth = 400; //in your case
-    double availableWidth = MediaQuery.of(context).size.width;
-
-    int i = availableWidth ~/ minTileWidth;
-    return i;
-  }
-
-  @override
-  void initState() {
-    ageFocus = FocusNode();
-    bmiFocus = FocusNode();
-    hbFocus = FocusNode();
-    cycleFocus = FocusNode();
-    marriageFocus = FocusNode();
-    abortionFocus = FocusNode();
-    fshFocus = FocusNode();
-    lhFocus = FocusNode();
-    thsFocus = FocusNode();
-    amhFocus = FocusNode();
-    vitFocus = FocusNode();
-    prgFocus = FocusNode();
-    sugarFocus = FocusNode();
-    bpFocus = FocusNode();
-    endFocus = FocusNode();
-    buttonFocus = FocusNode();
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
-    double w = MediaQuery.of(context).size.width;
-    double h = MediaQuery.of(context).size.height;
-    return Scaffold(
-      backgroundColor: WebColors.bodyColor,
-      body: SingleChildScrollView(
-        child: Column(
+    final Size size = MediaQuery.of(context).size;
+    return buildHomePersonalInfo(size);
+  }
+
+  Row buildHomePersonalInfo(Size size) {
+    return Row(
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(
-              width: w,
-              height: h * 0.2,
-              padding: const EdgeInsets.all(20),
-              decoration: const BoxDecoration(
-                color: WebColors.heading,
-              ),
-              child: Align(
-                alignment: Alignment.bottomLeft,
-                child: Text(
-                  Strings.heading,
-                  style: WebTextStyle.montserratStyle(
-                      color: WebColors.headingTextColor, fontSize: 50),
-                ),
-              ),
-            ),
-            SizedBox(
-              width: w,
-              height: h * 0.8,
-              child: GridView(
-                padding: const EdgeInsets.all(20),
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: getItemCountPerRow(context),
-                  mainAxisSpacing: 10,
-                  crossAxisSpacing: 10,
-                  childAspectRatio: 2.5,
-                ),
-                scrollDirection: Axis.vertical,
-                children: [
-                  InputField(
-                    inputFieldName: Strings.Age,
-                    textEditingController: textEditingControllerAge,
-                    fName: ageFocus,
-                    currentFocus: ageFocus,
-                    nextFocus: bmiFocus,
+            FadeInDown(
+                duration: const Duration(milliseconds: 1200),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 150.0),
+                  child: Text(
+                    Strings.projectName,
+                    style: WebTextStyle.montserratStyle(
+                        color: Colors.white, fontSize: 30),
                   ),
-                  InputField(
-                    inputFieldName: Strings.bmi,
-                    textEditingController: textEditingControllerBmi,
-                    fName: bmiFocus,
-                    currentFocus: bmiFocus,
-                    nextFocus: hbFocus,
-                  ),
-                  InputField(
-                    textEditingController: textEditingControllerHb,
-                    inputFieldName: Strings.hb,
-                    fName: hbFocus,
-                    currentFocus: hbFocus,
-                    nextFocus: cycleFocus,
-                  ),
-                  InputField(
-                    inputFieldName: Strings.cycleLength,
-                    textEditingController: textEditingControllerCycle,
-                    fName: cycleFocus,
-                    currentFocus: cycleFocus,
-                    nextFocus: marriageFocus,
-                  ),
-                  InputField(
-                    inputFieldName: Strings.marriageStatus,
-                    textEditingController: textEditingControllerMarriage,
-                    fName: marriageFocus,
-                    currentFocus: marriageFocus,
-                    nextFocus: abortionFocus,
-                  ),
-                  InputField(
-                    inputFieldName: Strings.noOfAbortion,
-                    textEditingController: textEditingControllerAbortion,
-                    fName: abortionFocus,
-                    currentFocus: abortionFocus,
-                    nextFocus: fshFocus,
-                  ),
-                  InputField(
-                    textEditingController: textEditingControllerFsh,
-                    inputFieldName: Strings.fsh,
-                    fName: fshFocus,
-                    currentFocus: fshFocus,
-                    nextFocus: lhFocus,
-                  ),
-                  InputField(
-                    inputFieldName: Strings.lh,
-                    textEditingController: textEditingControllerLh,
-                    fName: lhFocus,
-                    currentFocus: lhFocus,
-                    nextFocus: thsFocus,
-                  ),
-                  InputField(
-                    inputFieldName: Strings.ths,
-                    textEditingController: textEditingControllerThs,
-                    fName: thsFocus,
-                    currentFocus: thsFocus,
-                    nextFocus: amhFocus,
-                  ),
-                  InputField(
-                    inputFieldName: Strings.amh,
-                    textEditingController: textEditingControllerAmh,
-                    fName: amhFocus,
-                    currentFocus: amhFocus,
-                    nextFocus: vitFocus,
-                  ),
-                  InputField(
-                    inputFieldName: Strings.vit,
-                    textEditingController: textEditingControllerVit,
-                    fName: vitFocus,
-                    currentFocus: vitFocus,
-                    nextFocus: prgFocus,
-                  ),
-                  InputField(
-                    inputFieldName: Strings.prg,
-                    textEditingController: textEditingControllerPrg,
-                    fName: prgFocus,
-                    currentFocus: prgFocus,
-                    nextFocus: sugarFocus,
-                  ),
-                  InputField(
-                    inputFieldName: Strings.sugar,
-                    textEditingController: textEditingControllerSugar,
-                    fName: sugarFocus,
-                    currentFocus: sugarFocus,
-                    nextFocus: bpFocus,
-                  ),
-                  InputField(
-                    inputFieldName: Strings.bp,
-                    textEditingController: textEditingControllerBp,
-                    fName: bpFocus,
-                    currentFocus: bpFocus,
-                    nextFocus: endFocus,
-                  ),
-                  InputField(
-                    inputFieldName: Strings.endometrium,
-                    textEditingController: textEditingControllerEndometrium,
-                    fName: endFocus,
-                    currentFocus: endFocus,
-                    nextFocus: buttonFocus,
-                  )
-                ],
-              ),
-            ),
-            Container(
-              width: w,
-              height: h * 0.4,
-              margin: const EdgeInsets.all(20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  InkWell(
-                    onFocusChange: (value) {
-                      buttonFocused = value;
-                      setState(() {});
-                    },
-                    focusNode: buttonFocus,
-                    onHover: (hover) {
-                      setState(() {
-                        isHover = hover;
-                      });
-                    },
-                    onTap: () {
-                      final LinearModel linear = LinearModel(
-                        Age: textEditingControllerAge.text,
-                        bmi: textEditingControllerBmi.text,
-                        hb: textEditingControllerHb.text,
-                        cycleLength: textEditingControllerCycle.text,
-                        marriageStatus: textEditingControllerMarriage.text,
-                        noOfAbortion: textEditingControllerAbortion.text,
-                        fsh: textEditingControllerFsh.text,
-                        lh: textEditingControllerLh.text,
-                        ths: textEditingControllerThs.text,
-                        amh: textEditingControllerAmh.text,
-                        vit: textEditingControllerVit.text,
-                        prg: textEditingControllerPrg.text,
-                        sugar: textEditingControllerSugar.text,
-                        bp: textEditingControllerBp.text,
-                        endometrium: textEditingControllerEndometrium.text,
-                      );
-
-                      final LinearModel linearTest = LinearModel(
-                          Age: '28',
-                          bmi: '20.3',
-                          hb: '10.48',
-                          cycleLength: '5',
-                          marriageStatus: '20',
-                          noOfAbortion: '0',
-                          fsh: '10.95',
-                          lh: '3.67',
-                          ths: '0.68',
-                          amh: '2.07',
-                          vit: '17.1',
-                          prg: '0.57',
-                          sugar: '92',
-                          bp: '92',
-                          endometrium: '8.5');
-
-                      LinearRegression()
-                          .postLinearRegression(linear)
-                          .then((value) {
-                        result = value.toString();
-                        setState(() {});
-                      });
-                    },
-                    child: Container(
-                      width: w * 0.2,
-                      height: h * 0.1,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          border: Border.all(
-                              color: buttonFocused
-                                  ? Colors.black
-                                  : Colors.transparent),
-                          color: isHover ? Colors.blue : WebColors.heading),
-                      child: const Center(
-                        child: Text("Calculate And Review"),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    width: w * 0.6,
-                    height: h * 0.4,
-                    padding: const EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: WebColors.inputFieldBox),
-                    child: Text(
-                      "Results:- $result",
-                      style: WebTextStyle.headerTextStyle(
-                        color: Colors.black,
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            )
+                )),
           ],
         ),
-      ),
+      ],
     );
   }
 }
