8a6f847e9adb61ed52387375ca7a68a7eba780a0
yukthi14 <109909210+yukthi14@users.noreply.github.com>
2024-03-10 14:04:28 +0530

    changed the color pattern.

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,8 +1,11 @@
 import 'dart:ui';
 
 class WebColors {
-  static const Color headingTextColor = Color(0xfffff8ff);
-  static const Color heading = Color(0xff9933ff);
-  static const Color bodyColor = Color(0xffcf7ffc);
-  static const Color inputFieldBox = Color(0xfff7c5ff);
+  static const Color headingTextColor = Color(0xff66FCF1);
+  // static const Color heading = Color(0xff9933ff);
+  // static const Color bodyColor = Color(0xffcf7ffc);
+  static const Color inputFieldBox = Color(0xff1F2833);
+  static const Color color1 = Color(0xffC5C6C7);
+  static const Color appBar = Color(0xff0B0C10);
+  static const Color background = Color(0xff45A29E);
 }
