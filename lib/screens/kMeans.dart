2971ba8d084126ef849b57acd4589bba4367b596
yukthi14 <109909210+yukthi14@users.noreply.github.com>
2024-03-10 14:05:00 +0530

    created page for kmeans unsupervised method.

---
+++ lib/screens/kMeans.dart
@@ -0,0 +1,18 @@
+import 'package:detection_of_infertility_in_women_based_on_gynecological_data_using_supervised_and_unsupervised_method/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+class KMeansScreen extends StatefulWidget {
+  const KMeansScreen({super.key});
+
+  @override
+  State<KMeansScreen> createState() => _KMeansScreenState();
+}
+
+class _KMeansScreenState extends State<KMeansScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: WebColors.appBar,
+    );
+  }
+}
